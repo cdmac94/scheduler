5206171ce831db940f55988fa75e6e0b528c07fc
Collin MacLeod <collinmacleod79@gmail.com>
2022-01-23 17:47:11 -0800

    Added basic hook through test based development

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -4,16 +4,27 @@ import "components/Appointments/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import useVisualMode from "hooks/useVisualMode";
 
+const SHOW = "SHOW";
+const EMPTY = "EMPTY";
+const CREAT = "CREATE";
 export default function Appointment(props) {
 
   const {time, id, interview} = props;
 
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
-    <article className="appointment">
-      <Header time={time} />
-      {props.interview ? <Show id={id} student={interview.student}  interviewer={interview.interviewer.name}/> : <Empty />}
-    </article>
+    {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
   )
   
 }
